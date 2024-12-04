a9accc3b2f4ae632be5ccddb11fd3808b21c839d
mohi14 <mohiuddinkhan1409@gmail.com>
2024-12-04 23:17:55 +0100

    localization json file created

---
+++ app/[lang]/dictionaries/disctionaries.js
@@ -0,0 +1,9 @@
+import "server-only";
+
+const dictionaries = {
+  en: () => import("./en.json").then((module) => module.default),
+  de: () => import("./de.json").then((module) => module.default),
+  bn: () => import("./bn.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale) => dictionaries[locale]();
